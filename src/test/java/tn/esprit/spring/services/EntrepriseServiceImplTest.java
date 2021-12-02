c276d602f34dcd1da5f7f9b7fc3b0c1df3536b7b
DESKTOP-D3DJ5AT\lenovo <azib.donia96@gmail.com>
2021-12-02 09:33:20 +0100

    commit junit test part 5

--- src/test/java/tn/esprit/spring/services/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/services/EntrepriseServiceImplTest.java
@@ -119,7 +119,7 @@ import tn.esprit.spring.entities.Entreprise;
 	                //Entreprise addedEntreprise = ES.ajouterEntreprise(entreprise);
 	                ESer.ajouterEntreprise(entreprise);
 	                
-	          	  	Assert.assertTrue(ESer.deleteEntrepriseById(46));
+	          	  	Assert.assertTrue(ESer.deleteEntrepriseById(47));
 
 	                
 	                //List<Entreprise> listentreprisebefore=ESer.getallEntreprises();
